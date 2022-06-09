1d9864d4a8edc70d8a786ea6a07fe7e43580a2cf
godwin <godwin@fincra.com>
2022-06-09 11:42:56 +0100

    sfrt

--- resources/js/pages/Client/Index.vue
+++ resources/js/pages/Client/Index.vue
@@ -1,131 +1,156 @@
 <template>
-<section class="h_me">
-    <div class="home-col">
-
-        <div class="box" style="min-height: 500px !important">
-            <div class="hialXu">
-                <div class="hOOOSa">
-                    <span>total Balance</span>
-                </div>
-                <placeholder-div v-if="i_balance"></placeholder-div>
-                <div class="bIZTTX" v-else>
-                    <div class="dsdsds_1">{{ total }}</div>
-                    <div class="dsdsds_2">
-                        {{ this.crypto_balance
-                            }}<span class="xsdsd"> {{ this.crypto }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="_min_nav">
-                <span class="active" @click="getWalletBalance">
-                    Total
-                </span>
-                <span> Wallet </span>
-                <span> Hardware </span>
-            </div>
-            <div class="_boodyy">
-                <div class="ePAgsD">
-                    <div class="_mIRg">
-                        <div class="dfUVbO">
-                            <span class="_IcN">
-                                <div class="coin-icon">&#36;</div>
-                            </span>
-                            <span class="_TnxT"> U.S Dollars </span>
-                        </div>
-
-                        <placeholder-div v-if="i_balance"></placeholder-div>
-                        <div class="dqvKQV" v-else>
-                            <div class="dmeMyq">
-                                {{ balances.usd | addComma }}
-                            </div>
-                            <div class="kTdwvR">{{ balances.btc }} BTC</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="ePAgsD">
-                    <div class="_mIRg">
-                        <div class="dfUVbO">
-                            <span class="_IcN">
-                                <div class="coin-icon">&pound;</div>
-                            </span>
-                            <span class="_TnxT">British Pounds </span>
-                        </div>
-
-                        <placeholder-div v-if="i_balance"></placeholder-div>
-                        <div class="dqvKQV" v-else>
-                            <div class="dmeMyq">
-                                {{ balances.gbp | addComma }}
-                            </div>
-
-                            <div class="kTdwvR">{{ balances.btc }} BTC</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="ePAgsD">
-                    <div class="_mIRg">
-                        <div class="dfUVbO">
-                            <span class="_IcN">
-                                <div class="coin-icon">&euro;</div>
-                            </span>
-                            <span class="_TnxT"> Euros </span>
-                        </div>
-
-                        <placeholder-div v-if="i_balance"></placeholder-div>
-                        <div class="dqvKQV" v-else>
-                            <div class="dmeMyq">
-                                {{ balances.eur | addComma }}
-                            </div>
-
-                            <div class="kTdwvR">{{ balances.btc }} BTC</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="ePAgsD" style="padding-bottom: 65px">
-                    <div class="_mIRg">
-                        <div class="dfUVbO">
-                            <span class="_IcN">
-                                <div class="coin-icon">&yen;</div>
-                            </span>
-                            <span class="_TnxT"> Chinese Yen </span>
-                        </div>
-
-                        <placeholder-div v-if="i_balance"></placeholder-div>
-                        <div class="dqvKQV" v-else>
-                            <div class="dmeMyq">
-                                {{ balances.jpy | addComma }}
-                            </div>
-
-                            <div class="kTdwvR">{{ balances.btc }} BTC</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <section class="y_me">
+        <div class="controlBtnss">
+            <button
+                class="gzzwuM"
+                @click="$store.dispatch('sendBitcoin', true)"
+            >
+                Send
+            </button>
+            <button
+                class="gzzwuM"
+                @click="$store.dispatch('recieveBitcoin', true)"
+            >
+                Recieve
+            </button>
         </div>
-    </div>
+        <section class="h_me">
+            <div class="home-col">
+                <div class="box" style="min-height: 500px !important">
+                    <div class="hialXu">
+                        <div class="hOOOSa">
+                            <span>total Balance</span>
+                        </div>
+                        <placeholder-div v-if="i_balance"></placeholder-div>
+                        <div class="bIZTTX" v-else>
+                            <div class="dsdsds_1">{{ total }}</div>
+                            <div class="dsdsds_2">
+                                {{ this.crypto_balance
+                                }}<span class="xsdsd"> {{ this.crypto }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="_min_nav">
+                        <span class="active" @click="getWalletBalance">
+                            Total
+                        </span>
+                        <span> Wallet </span>
+                        <span> Hardware </span>
+                    </div>
+                    <div class="_boodyy">
+                        <div class="ePAgsD">
+                            <div class="_mIRg">
+                                <div class="dfUVbO">
+                                    <span class="_IcN">
+                                        <div class="coin-icon">&#36;</div>
+                                    </span>
+                                    <span class="_TnxT"> U.S Dollars </span>
+                                </div>
 
-    <div class="home-col maxi">
-        <div class="box widget basNas">
-            <div class="controlBtns">
-                <button class="gzzwuM" @click="$store.dispatch('sendBitcoin', true)">
-                    Send
-                </button>
-                <button class="gzzwuM" @click="$store.dispatch('recieveBitcoin', true)">
-                    Recieve
-                </button>
+                                <placeholder-div
+                                    v-if="i_balance"
+                                ></placeholder-div>
+                                <div class="dqvKQV" v-else>
+                                    <div class="dmeMyq">
+                                        {{ balances.usd | addComma }}
+                                    </div>
+                                    <div class="kTdwvR">
+                                        {{ balances.btc }} BTC
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ePAgsD">
+                            <div class="_mIRg">
+                                <div class="dfUVbO">
+                                    <span class="_IcN">
+                                        <div class="coin-icon">&pound;</div>
+                                    </span>
+                                    <span class="_TnxT">British Pounds </span>
+                                </div>
+
+                                <placeholder-div
+                                    v-if="i_balance"
+                                ></placeholder-div>
+                                <div class="dqvKQV" v-else>
+                                    <div class="dmeMyq">
+                                        {{ balances.gbp | addComma }}
+                                    </div>
+
+                                    <div class="kTdwvR">
+                                        {{ balances.btc }} BTC
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ePAgsD">
+                            <div class="_mIRg">
+                                <div class="dfUVbO">
+                                    <span class="_IcN">
+                                        <div class="coin-icon">&euro;</div>
+                                    </span>
+                                    <span class="_TnxT"> Euros </span>
+                                </div>
+
+                                <placeholder-div
+                                    v-if="i_balance"
+                                ></placeholder-div>
+                                <div class="dqvKQV" v-else>
+                                    <div class="dmeMyq">
+                                        {{ balances.eur | addComma }}
+                                    </div>
+
+                                    <div class="kTdwvR">
+                                        {{ balances.btc }} BTC
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ePAgsD" style="padding-bottom: 65px">
+                            <div class="_mIRg">
+                                <div class="dfUVbO">
+                                    <span class="_IcN">
+                                        <div class="coin-icon">&yen;</div>
+                                    </span>
+                                    <span class="_TnxT"> Chinese Yen </span>
+                                </div>
+
+                                <placeholder-div
+                                    v-if="i_balance"
+                                ></placeholder-div>
+                                <div class="dqvKQV" v-else>
+                                    <div class="dmeMyq">
+                                        {{ balances.jpy | addComma }}
+                                    </div>
+
+                                    <div class="kTdwvR">
+                                        {{ balances.btc }} BTC
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <placeholder-div v-if="i_frame"></placeholder-div>
-            <iframe @load="i_frame = false" :src="'/widget/' + crypto" id="content" class="iframe_a" />
-        </div>
-    </div>
-</section>
+            <div class="home-col maxi">
+                <div class="box widget basNas">
+                    <placeholder-div v-if="i_frame"></placeholder-div>
+                    <iframe
+                        @load="i_frame = false"
+                        :src="'/widget/' + crypto"
+                        id="content"
+                        class="iframe_a"
+                    />
+                </div>
+            </div>
+        </section>
+    </section>
 </template>
 
 <script>
 import PlaceholderDiv from "../../components/PlaceholderDiv";
-import {
-    mapGetters
-} from "vuex";
+import { mapGetters } from "vuex";
 export default {
     components: {
         PlaceholderDiv,
